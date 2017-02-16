67cc9b661cadf0330d79191f93f8029c8a37337c
Jacob Hartzer <JacobHartzer@gmail.com>
2017-02-15 18:36:15 -0600

    Added documentation

--- Phase 1/Code/t2top_speed.m
+++ Phase 1/Code/t2top_speed.m
@@ -1,15 +1,16 @@
 function [t_top] = t2top_speed(t_60,v_top)
     %t2top_speed - Function for estimating time to reach top speed
+    %   from 0-60 time and top speed 
     %
     %   USAGE
     %[t_top] = t2top_speed(t_60,v_top)
     %
     %   INPUT
-    %t_60       The vehicle's 0 to 60 (mph) time  
+    %t_60       The vehicle's 0 to 60 (mph) time (sec)
     %v_top      The vehicle's top speed (mph)
     %
     %   OUTPUT
-    %t_top      The estimated time to reach the top speed
+    %t_top      The estimated time to reach the top speed (sec)
 
     t_top = pi*t_60 / acos((v_top-120)/v_top);
     
